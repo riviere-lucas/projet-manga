ed8bd01da92c288b0d021512fa2423a6f949d7b6
Riviere Lucas <lucas.riviere@viacesi.fr>
2022-01-13 17:48:13 +0100

    ajout du header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
